8b416f6e132b6613d91f20a6cd1646afd89a136b
aseel-alsayeh <aseelalsayeh97@gmail.com>
2021-04-28 23:48:35 +0300

    edit q7

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
-'use strict'
+ 'use strict'
 let Name = prompt(" hello user, enter your name please ");
 alert ( "Welcome " + Name );
 // console.log("welcome  "+ Name);
 alert(" Be ready for Y/N questions :) " );
+
+let score = 0;
 
 function Q1(){
 let q1 = prompt(" Can we mix the oil with water or vice versa ? ( yes or no ) ").toLowerCase();
@@ -10,6 +12,7 @@ switch (q1) {
     case 'yes':
     //console.log("correct answer , yes we can by adding an emulsifier");    
     alert("correct answer , yes we can by adding an emulsifier" );
+    score++;
     break;
     case 'no':
     alert(" Incorrect answer, yes we can by adding an emulsifier");
@@ -19,8 +22,8 @@ switch (q1) {
     //console.log('dear '+ Name + ' please answer yes or no');    
     alert('dear '+ Name + " please answer yes or no");
     break;
-    return;
-}
+    
+}}
 Q1();
 
 function Q2(){
@@ -29,7 +32,9 @@ switch (q2) {
     case 'yes':
     //console.log("correct answer , yes the same in chemical composition just.");    
     alert("correct answer , yes the same in chemical composition just." );
+    score++;
     break;
+    
     case 'no':
     alert(" Incorrect answer, yes the same in chemical composition just.");
     //console.log("Incorrect answer, yes the same in chemical composition just.");
@@ -48,6 +53,7 @@ switch (q3) {
     case 'yes':
     //console.log("correct answer , yes there is a small amount of benzene ring,(0-2.5%) ");    
     alert("correct answer , yes there is a small amount of benzene ring,(0-2.5%)." );
+    score++;
     break;
     case 'no':
     alert(" Incorrect answer, yes there is a small amount of benzene ring,(0-2.5%).");
@@ -65,10 +71,11 @@ Q3();
 
 function Q4(){
 let q4 = prompt(" Can we convert Lead into Gold ? ( yes or no ) ").toLowerCase();
-switch (q3) {
+switch (q4) {
     case 'yes':
     //console.log("correct answer , yes but it is impractical) ");    
     alert("correct answer , yes but it is impractical." );
+    score++;
     break;
     case 'no':
     alert(" Incorrect answer, yes but it is impractical");
@@ -78,19 +85,20 @@ switch (q3) {
     //console.log('dear '+ Name + ' please answer yes or no');    
     alert('dear '+ Name + " please answer yes or no")
     break;
-return; } }
+ } }
 Q4();
 
 
 function Q5(){
 let q5 = prompt(" if we add salt to complete cup of water , does the amount increase and overfolowing  ? ( yes or no ) ").toLowerCase();
-switch (q3) {
+switch (q5) {
     case 'yes':
     //console.log(" Incorrect answer , No it will not overflow ");    
     alert("Incorrect answer , No it will not overflow ." );
     break;
     case 'no':
     alert(" correct answer, No it will not overflow");
+    score++;
     //console.log("correct answer, No it will not overflow");
     break; 
     default:
@@ -108,6 +116,7 @@ function Q6(){
     let q6 = Number(prompt( " guess a number (enter the number belwo) "));
     if (q6 ===7) {
         alert("you are lucky ! correct answer");
+        score++;
         break;
         break;
     } else if (q6 > 7){
@@ -116,30 +125,32 @@ function Q6(){
         alert("the number is too low ");
 
     }
-    alert("all attempts have been exausted , the correct answer is 7 ");
 }
+alert("all attempts have been exausted , the correct answer is 7 ");
 return; }
 Q6();
 
-         
 function Q7(){
-let k = 0;
-var j = 0;
-let i = 0;
-let color = ['red' , 'yellow' , 'orange'];
-let q7 = prompt("what is my fav color? ");
-while (j < 5){
-  
-    if ( q7 === color[0] || q7 === color[1] || q7===[2]){
-    alert("correct answer :) ");break;}
-  
-   if ( q7 !== color[0] || q7 === color[1] || q7===color[2]){
-      q7 = prompt('try again');
-      alert(" not the correct answer, please try again");
-      j++;
-    }
-     
-  }
-  alert ('the all correct answer are yellow , orange and red') ; }
-  return; }
-  Q7();
+    let color = ['red' , 'yellow' , 'orange'];
+    let flag = false;
+    for(let j=1 ; j<= 6; j++) {
+      let q7 = prompt("what is my fav color? ");// blue
+        for(let a = 0; a < color.length ; a++)
+        if ( q7 === color[a]) {
+        alert("correct answer :) ");
+        score++;
+        flag = true;
+        break;}
+    
+        if (flag === true){
+            break; }
+    
+        
+        }
+         
+      
+      alert ('the all correct answer are yellow , orange and red') ; }
+      
+      Q7(); 
+
+      document.write(" Your score is " + score);
